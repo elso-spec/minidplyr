844d680ed8feca8a36fed318da51fe8325d01d53
Elisabeth Solmunde <elisabeth.solmunde@gmail.com>
2025-08-21 10:48:11 +0200

    function select

---
+++ R/select.R
@@ -0,0 +1,31 @@
+
+#' Select
+#'
+#' @param data A data frame
+#' @param vars Variables (character strings or column indices)
+#'
+#' @returns A subsetted data frame
+#' @export
+#'
+#' @examples
+select <- function(data, vars) {
+  # data: a data frame
+  # vars: character vector of column names OR numeric indices
+
+  if (is.character(vars)) {
+    if (!all(vars %in% names(data))) {
+      stop("Some variables are not found in the data frame")
+    }
+    return(data[, vars, drop = FALSE])
+
+  } else if (is.numeric(vars)) {
+    if (any(vars < 1 | vars > ncol(data))) {
+      stop("Some indices are out of range")
+    }
+    return(data[, vars, drop = FALSE])
+
+  } else {
+    stop("vars must be either character column names or numeric indices")
+  }
+}
+
